1dba483d519829c06ab6041e5e7b4b787caff462
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-14 13:31:34 +0200

    Implemented properties mapping

---
+++ helpers/map-products-props.js
@@ -0,0 +1,27 @@
+const propsMap = require('../data/props-map.js')
+
+module.exports = (products) => {
+  return products.map(product => {
+    return {
+      title: '',
+      sku: '',
+      stocks: {},
+      ...Object.fromEntries(mapCollmexData(Object.entries(product)))
+    }
+  })
+}
+
+function mapCollmexData (productEntries) {
+  return productEntries.map(parseProductEntry).filter(entry => entry)
+}
+
+function parseProductEntry (entry) {
+  const propMap = propsMap[entry[0]]
+  if (!propMap) {
+    return undefined
+  }
+  const fieldType = propMap.type
+  const value = entry[1]
+  const parsedValue = fieldType === Array ? [value] : fieldType(value)
+  return [propMap.field, parsedValue]
+}
