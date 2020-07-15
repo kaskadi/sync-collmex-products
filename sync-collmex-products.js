c28e3c0876ddb38c58d276a17c469de207e1392b
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-15 15:41:08 +0200

    Hotfix: updated logging + error handling

--- sync-collmex-products.js
+++ sync-collmex-products.js
@@ -3,9 +3,16 @@ const mapProductsProps = require('./helpers/map-products-props.js')
 const setProducts = require('./helpers/set-products.js')
 
 module.exports.handler = async (event) => {
+  let res = true
   await getCollmexProducts()
     .then(mapProductsProps)
     .then(setProducts)
-    .finally(console.log)
-  return true
+    .then(bulkResponse => {
+      console.log(JSON.stringify(bulkResponse, null, 2))
+    })
+    .catch(err => {
+      console.log(JSON.stringify(err, null, 2))
+      res = false
+    })
+  return res
 }
